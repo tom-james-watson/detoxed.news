4788a9de4b3fdabcfea0b4938c9b3f7cc5e5e2a1
David Brandon <safe.plan8831@fastmail.com>
2022-01-24 17:02:01 -0500

    Generate an atom feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,28 @@
 const path = require("path");
 const postcss = require("postcss");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPassthroughCopy("static");
-
+  eleventyConfig.addFilter("parseWikiDate", function(s) {
+    year = new Date(Date.now()).getFullYear();
+    return new Date(Date.parse(s + ", " + year));
+  });
+  eleventyConfig.addFilter("makeEntryTitle", function(entry) {
+    if (entry.ogMetadata) {
+      if (entry.ogMetadata.ogTitle) {
+        return entry.ogMetadata.ogTitle;
+      }
+      if (entry.ogMetadata.ogDescription) {
+        return entry.ogMetadata.ogDescription;
+      }
+    }
+    if (entry.tags.length > 0) {
+      return entry.tags[0].name;
+    }
+    return "No title";
+  });
   eleventyConfig.addPairedShortcode("postcss", async (code) => {
     // for relative path CSS imports
     const filepath = path.join(__dirname, "_includes/style.css");
