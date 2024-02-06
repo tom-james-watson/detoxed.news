94a77260d5007072b3439f325818d343ec36b8de
Tom Watson <tom@tomjwatson.com>
2024-02-06 13:34:00 +0100

    Bump dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,21 @@
 const path = require("path");
-const postcss = require("postcss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("static");
 
-  eleventyConfig.addPairedShortcode("postcss", async (code) => {
-    // for relative path CSS imports
-    const filepath = path.join(__dirname, "_includes/style.css");
+  // eleventyConfig.addPairedShortcode("postcss", async (code) => {
+  //   // for relative path CSS imports
+  //   const filepath = path.join(__dirname, "_includes/style.css");
 
-    return await postcss([
-      // PostCSS plugins
-      require("postcss-import"),
-      require("precss"),
-      require("autoprefixer"),
-      require("cssnano"),
-    ])
-      .process(code, { from: filepath })
-      .then((result) => result.css);
-  });
+  //   return await postcss([
+  //     // PostCSS plugins
+  //     require("postcss-import"),
+  //     require("precss"),
+  //     require("cssnano"),
+  //   ])
+  //     .process(code, { from: filepath })
+  //     .then((result) => result.css);
+  // });
 };
 
 require("ts-node").register({
